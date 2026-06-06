0292b1b768de4cacfa29c92f147313b5e9f3a140
Rexxwell Putra Tendean <152246711+Rexxwell@users.noreply.github.com>
2026-06-06 16:48:12 +0700

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,8 @@
+class Solution:
+    # Brute Force
+    def isValid(self, s: str) -> bool:
+        while ("()" in s) or ("[]" in s) or ("{}" in s):
+            s = s.replace("()", "")
+            s = s.replace("[]", "")
+            s = s.replace("{}", "")
+        return s == ""
