1e55319e004e8176b03fecf1d5624d0fdae98206
Rexxwell Putra Tendean <152246711+Rexxwell@users.noreply.github.com>
2026-06-10 11:57:09 +0700

    Add: linked-list-cycle-detection - submission-3

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-3.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    # Visited List (Hash Set)
+    # Runtime: 28ms
+    # Memory: 8.0 MB
+    # Time Complexity: O(n^2)
+    # Space Complexity: O(n)
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = []
+        currNode = head
+        while (currNode != None):
+            if (currNode in visited):
+                return True
+            else:
+                visited.append(currNode)
+            currNode = currNode.next
+        return False
