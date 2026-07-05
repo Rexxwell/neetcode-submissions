9d131242a29159c9915ffed410ce88d1f0ab2759
Rexxwell Putra Tendean <152246711+Rexxwell@users.noreply.github.com>
2026-07-05 21:00:46 +0700

    Add: counting-bits - submission-6

---
+++ Data Structures & Algorithms/counting-bits/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    # Manual Bit Checking
+    # Runtime: 30ms
+    # Memory: 7.7 MB
+    # Time Complexity: O(nlog(n))
+    # Space Complexity: O(n)
+    def countBits(self, n: int) -> List[int]:
+        output = []
+        for i in range(n + 1):
+            result = 0
+            while (i > 0):
+                if (i % 2 == 1):
+                    result += 1
+                i >>= 1 # i //= 2 also works here
+            output.append(result)
+        return output
