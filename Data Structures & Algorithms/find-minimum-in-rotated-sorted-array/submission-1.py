b0f06f53f9731967ea9717f158896397e54cea73
Rexxwell Putra Tendean <152246711+Rexxwell@users.noreply.github.com>
2026-08-15 23:20:51 +0800

    Add: find-minimum-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    # Brute Force
+    # Runtime: 30ms
+    # Memory: 7.7 MB
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    def findMin(self, nums: List[int]) -> int:
+        return min(nums)
