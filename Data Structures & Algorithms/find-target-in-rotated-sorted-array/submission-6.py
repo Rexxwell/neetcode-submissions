c59bbe1bd695a0c461c38bfd8c22d682e2267972
Rexxwell Putra Tendean <152246711+Rexxwell@users.noreply.github.com>
2026-08-16 23:30:39 +0800

    Add: find-target-in-rotated-sorted-array - submission-6

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-6.py
@@ -0,0 +1,44 @@
+class Solution:
+    # Binary Search (Finding the cut)
+    # Runtime: 29ms
+    # Memory: 7.9 MB
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+
+        while low < high:
+            mid = low + (high - low) // 2
+            num_mid = nums[mid]
+            num_high = nums[high]
+
+            if num_mid > num_high:
+                # low and mid are in the same segment
+                # high is in a different segment
+                # minimum element must be in the right segment
+                low = mid + 1
+            elif num_mid <= num_high:
+                # low is in a different segment
+                # mid and high are in the same segment
+                # minimum element must in the left segment or mid itself
+                high = mid
+
+        if target in nums[low:len(nums) + 1]:
+            high = len(nums) - 1
+        elif target in nums[:low]:
+            low = 0
+            high -= 1
+        else:
+            return -1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+            num_mid = nums[mid]
+
+            if num_mid == target:
+                return mid
+            elif num_mid < target:
+                low = mid + 1
+            elif num_mid > target:
+                high = mid - 1
